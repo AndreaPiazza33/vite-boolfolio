d9a759246f4ef926acf8ff4adca867776c90b5f5
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-11-08 17:29:36 +0100

    feat: add axios and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import "./assets/scss/style.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
