cb244d1652c83a023543a1795b7d1aa5c1d0952d
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-11-08 17:29:49 +0100

    feat: add api call

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+
+      api: {
+        baseUrl: "http://127.0.0.1:8000/api/",
+      },
+    };
+  },
+
+  components: { ProjectList },
+
+  methods: {
+    fetchProjects(uri = this.api.baseUrl + "projects") {
+      axios.get(uri).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
   },
 };
 </script>
 <template>
   <h1>HELLO</h1>
+  <hr />
+  <ProjectList></ProjectList>
 </template>
-<style></style>
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h2>PostList</h2>
+</template>
+
+<style lang="scss" scoped></style>
