4dab925caa1ea2ba9393a340613196d3cea866ee
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-11-09 17:34:50 +0100

    feat: add store

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script>
 import ProjectList from "./components/projects/ProjectList.vue";
 import axios from "axios";
+import { store } from "./data/store";
 
 export default {
   data() {
     return {
       projects: [],
-
-      api: {
-        baseUrl: "http://127.0.0.1:8000/api/",
-      },
     };
   },
 
   components: { ProjectList },
 
   methods: {
-    fetchProjects(uri = this.api.baseUrl + "projects") {
+    fetchProjects(uri = store.api.baseUrl + "projects") {
       axios.get(uri).then((response) => {
         this.projects = response.data.data;
       });
